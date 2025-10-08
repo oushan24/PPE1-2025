4aafe4015d3ecdeb87722b556d5a67da91dd2d66
oushan24 <annushmarina@gmail.com>
2025-10-08 11:55:13 +0200

    Ajout du script

---
+++ Exercices/comptes.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+echo "argument donné : $1"
+
+CHEMIN=$1
+
+echo "Nombre de lieux en 2016:" 
+cat "$CHEMIN/2016/"* | grep Location |  wc -l
+echo "Nombre de lieux en 2017:"
+cat "$CHEMIN/2017/"* | grep Location |  wc -l
+echo "Nombre de lieux en 2018:"
+cat "$CHEMIN/2018/"* | grep Location |  wc -l
