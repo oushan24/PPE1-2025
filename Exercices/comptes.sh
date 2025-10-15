cb8b9d0ecb153ff1e1551a10f9d8ae355e857ca2
oushan24 <annushmarina@gmail.com>
2025-10-15 11:55:54 +0200

    Ajout d'exercice 4

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -4,9 +4,8 @@ echo "argument donné : $1"
 
 CHEMIN=$1
 
-echo "Nombre de lieux en 2016:" 
-cat "$CHEMIN/2016/"* | grep Location |  wc -l
-echo "Nombre de lieux en 2017:"
-cat "$CHEMIN/2017/"* | grep Location |  wc -l
-echo "Nombre de lieux en 2018:"
-cat "$CHEMIN/2018/"* | grep Location |  wc -l
+for annee in 2016 2017 2018
+do
+    echo "Nombre de lieux en $annee:" 
+    cat "$CHEMIN/$annee/"* | grep Location |  wc -l
+done
